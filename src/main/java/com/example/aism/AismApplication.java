aa4d6ba0f5b1b69842b1c0a6dad0dc2617d21160
leman <lemanh8456@gmail.com>
2024-03-13 15:45:04 +0700

    add plugin

---
+++ src/main/java/com/example/aism/AismApplication.java
@@ -0,0 +1,13 @@
+package com.example.aism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AismApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aism/AismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
